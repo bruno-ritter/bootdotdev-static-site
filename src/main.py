49096d1d8528d55e2111b50493cefbb1838b41b0
bruno-ritter <bruno.maritter@gmail.com>
2026-06-29 21:59:07 -0300

    CH5: Deploy

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import os
 import shutil
+import sys
 from pathlib import Path
 
 from markdown_to_html import extract_title, markdown_to_html_node
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dest_dir_path):
         os.mkdir(dest_dir_path)
 
@@ -14,12 +15,12 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
-def generate_page(from_path, template_path, dest_path) -> None:
+def generate_page(from_path, template_path, dest_path, basepath) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r", encoding="utf-8") as f:
         md: str = f.read()
@@ -29,15 +30,18 @@ def generate_page(from_path, template_path, dest_path) -> None:
     html_node = markdown_to_html_node(md)
     html_str = html_node.to_html()
     title_page: str | None = extract_title(md)
-    content: str = template.replace("{{ Title }}", title_page).replace(
-        "{{ Content }}", html_str
+    content: str = (
+        template.replace("{{ Title }}", title_page)
+        .replace("{{ Content }}", html_str)
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
     )
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(content)
 
 
-def static_to_public(source_path: str, destination_path: str) -> None:
+def copy_files(source_path: str, destination_path: str) -> None:
     source_path = os.path.abspath(source_path)
     destination_path = os.path.abspath(destination_path)
 
@@ -50,15 +54,18 @@ def static_to_public(source_path: str, destination_path: str) -> None:
         if os.path.isfile(from_path):
             shutil.copy(from_path, dest_path)
         else:
-            static_to_public(from_path, dest_path)
+            copy_files(from_path, dest_path)
 
 
 def main() -> None:
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
-    static_to_public("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    print(basepath)
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
+    os.mkdir("docs")
+    copy_files("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 if __name__ == "__main__":
